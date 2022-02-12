aed071021b16c4461ddab7ab8b7f474268b61305
mlarocca <97350905+mlaroccaFIU@users.noreply.github.com>
2022-02-12 13:27:30 -0500

    Added IR class

---
+++ src/main/java/com/group3/iguanapatrol/InfraredDetector.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.group3.iguanapatrol;
+
+/**
+ * @author Michael La Rocca
+ *
+ */
+public class InfraredDetector {
+
+	private String passiveInfraredSensor;
+	private int resolution;
+	private int lightIntensity;
+	private int chargeState;
+	private String motionDetection;
+	
+	public void pairHun() {
+		//
+	}
+	
+	public void detectMotion() {
+		//
+	}
+	
+	public void captureObject() {
+		//
+	}
+	
+	public void captureHeatPattern() {
+		//
+	}
+	
+	public void sendHubInraredReading() {
+		//
+	}
+	
+	public void sendLowBattery() {
+		//
+	}
+}
